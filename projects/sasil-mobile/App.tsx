ad9e8d57a97ca3649ce12e14bd066052755f73bf
kanghyun98 <lkh981107@gmail.com>
2022-02-20 01:27:45 +0900

    feat: add eslint, prettier, husky settings

--- projects/sasil-mobile/App.tsx
+++ projects/sasil-mobile/App.tsx
@@ -1,27 +1,31 @@
 import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { testFetch } from '@sasil/common';
+
+const COLOR = '#fff';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: COLOR,
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default function App() {
   const onPress = useCallback(async () => {
     const result = await testFetch(2);
-    alert(result.data.email);
-  }, [])
+    Alert.alert(result.data.email);
+  }, []);
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={onPress}><Text>asdf</Text></TouchableOpacity>
+      <TouchableOpacity onPress={onPress}>
+        <Text>asdf</Text>
+      </TouchableOpacity>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
