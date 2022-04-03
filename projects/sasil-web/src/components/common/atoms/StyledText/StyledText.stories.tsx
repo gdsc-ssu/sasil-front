7886717f17183ad6a30099d0b92bf829d960eceb
ballsona0925 <ballsona0925@naver.com>
2022-04-03 14:58:17 +0900

    feat: add StyledText in common/atom

---
+++ projects/sasil-web/src/components/common/atoms/StyledText/StyledText.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import COLORS from '@/constants/styles/color';
+import { TEXT_STYLE_NAME } from '@/constants/styles/textStyles';
+import StyledText, { StyledTextProps } from './StyledText';
+
+export default {
+  title: 'components/common/atom/StyledText',
+  component: StyledText,
+  argTypes: {
+    color: {
+      options: COLORS.grayscale,
+      control: 'select',
+    },
+    textStyleName: {
+      options: TEXT_STYLE_NAME,
+      control: 'select',
+    },
+  },
+} as ComponentMeta<typeof StyledText>;
+
+const Template: ComponentStory<typeof StyledText> = ({
+  color,
+  textStyleName,
+}: StyledTextProps) => (
+  <StyledText color={color} textStyleName={textStyleName}>
+    Styled Text
+  </StyledText>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  color: '#000',
+  textStyleName: TEXT_STYLE_NAME.title,
+};
---
+++ projects/sasil-web/src/components/common/atoms/StyledText/StyledText.tsx
@@ -0,0 +1,17 @@
+import COLORS from '@/constants/styles/color';
+import { TextStyleName, TEXT_STYLES } from '@/constants/styles/textStyles';
+import styled from '@emotion/styled';
+
+export interface StyledTextProps {
+  color?: string;
+  textStyleName: TextStyleName;
+}
+
+const StyledText = styled.div<StyledTextProps>((props) => ({
+  color: props.color || COLORS.grayscale.black,
+  fontSize: TEXT_STYLES[props.textStyleName].fontSize,
+  fontFamily: TEXT_STYLES[props.textStyleName].fontFamily,
+  fontWeight: TEXT_STYLES[props.textStyleName].fontWeight,
+}));
+
+export default StyledText;
