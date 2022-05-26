42db9b45edb70ce66d2cf4e8adb7f47dc9782f14
kanghyun98 <lkh981107@gmail.com>
2022-05-27 01:01:20 +0900

    feat: activate search in TopBar component

--- projects/sasil-web/src/components/templates/TopBar/TopBar.tsx
+++ projects/sasil-web/src/components/templates/TopBar/TopBar.tsx
@@ -1,18 +1,32 @@
+import React, { useCallback } from 'react';
+import { useRouter } from 'next/router';
+
+import { useInputChange } from '@/logics/hooks/useInputChange';
 import Navigation from '@/components/organisms/Navigation';
 import ProfileImage from '@/components/atoms/ProfileImage';
 import SearchBar from '@/components/molelcules/SearchBar';
-
 import * as styles from './TopBar.style';
 
 const TopBar = () => {
-  const handleChange = () => {};
-  const handleSearch = () => {};
+  const router = useRouter();
+  const [searchVal, , handleChangeSearchVal] = useInputChange('');
+
+  const handleSearch = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (searchVal.trim().length > 0) {
+        router.push('/'); // TODO: 검색 결과 URL
+      }
+    },
+    [router, searchVal],
+  );
 
   return (
     <styles.styledTopBar>
       <SearchBar
         className="search-bar"
-        onChange={handleChange}
+        onChange={handleChangeSearchVal}
         onSearch={handleSearch}
       />
       <styles.sideMenu>
