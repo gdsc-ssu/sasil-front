45b0349258b9e39e35873b305c89f4a23d744485
ballsona0925 <ballsona0925@naver.com>
2022-05-28 14:45:52 +0900

    fix: remove async storage in storybook

--- projects/sasil-mobile/storybook/index.js
+++ projects/sasil-mobile/storybook/index.js
@@ -32,7 +32,8 @@ configure(() => {
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({
-  asyncStorage: require('@react-native-async-storage/async-storage').default,
+  // asyncStorage: require('@react-native-async-storage/async-storage').default,
+  asyncStorage: null,
 });
 
 export default StorybookUIRoot;
