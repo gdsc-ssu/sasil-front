af501c7c1bfbd24b0097a0d58885b3a660e5e4a0
kanghyun98 <lkh981107@gmail.com>
2022-05-30 03:43:47 +0900

    refactor: separate style's props from component's props

--- projects/sasil-web/src/components/atoms/StyledText/StyledText.style.tsx
+++ projects/sasil-web/src/components/atoms/StyledText/StyledText.style.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
 import styled from '@emotion/styled';
 
 import { COLORS, TEXT_STYLES_PC, TextStyleName } from '@sasil/common';
 
-export interface StyledTextProps {
-  children: React.ReactNode;
-  /** 텍스트 색 (인자를 지정해주지 않을 시, 기본값은 black) */
+interface WrapProps {
   color?: string;
   /** 텍스트 스타일 종류로, TEXT_STYLE_NAME.* 로 지정된다. */
   textStyleName: TextStyleName;
-  /** 컴포넌트로 생성할 요소의 클래스명 */
-  className?: string;
 }
 
-export const wrap = styled.div<StyledTextProps>((props) => ({
-  color: props.color || COLORS.grayscale.black,
-  fontSize: TEXT_STYLES_PC[props.textStyleName].fontSize,
-  fontWeight: TEXT_STYLES_PC[props.textStyleName].fontWeight,
-  lineHeight: `${TEXT_STYLES_PC[props.textStyleName].lineHeight}px`,
+export const wrap = styled.div(({ color, textStyleName }: WrapProps) => ({
+  color: color || COLORS.grayscale.black,
+  fontSize: TEXT_STYLES_PC[textStyleName].fontSize,
+  fontWeight: TEXT_STYLES_PC[textStyleName].fontWeight,
+  lineHeight: `${TEXT_STYLES_PC[textStyleName].lineHeight}px`,
 }));
--- projects/sasil-web/src/components/atoms/StyledText/StyledText.tsx
+++ projects/sasil-web/src/components/atoms/StyledText/StyledText.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+
+import { TextStyleName } from '@sasil/common';
 import * as styles from './StyledText.style';
-import { StyledTextProps } from './StyledText.style';
+
+export interface StyledTextProps {
+  children: React.ReactNode;
+  /** 텍스트 색 (인자를 지정해주지 않을 시, 기본값은 black) */
+  color?: string;
+  /** 텍스트 스타일 종류로, TEXT_STYLE_NAME.* 로 지정된다. */
+  textStyleName: TextStyleName;
+  /** 컴포넌트로 생성할 요소의 클래스명 */
+  className?: string;
+}
 
 /**
  * 주어진 인자를 바탕으로 style이 적용된 text를 생성하는 컴포넌트
