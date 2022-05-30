e47b6ad96a3ac1533b8ae93c1b4780d9ba938475
kanghyun98 <lkh981107@gmail.com>
2022-05-31 02:43:50 +0900

    feat: add NavItem component

---
+++ projects/sasil-mobile/src/components/organisms/Navigation/NavItem.style.tsx
@@ -0,0 +1,35 @@
+import styled, { css } from '@emotion/native';
+import { COLORS } from '@sasil/common';
+
+interface StyledNavItemProps {
+  isFocused: boolean;
+}
+
+export const styledNavItem = styled.Pressable(
+  ({ isFocused }: StyledNavItemProps) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flex: 1,
+    maxHeight: 59,
+    borderTopWidth: 2,
+    borderTopColor: isFocused ? COLORS.primary.normal : 'transparent',
+  }),
+);
+
+export const iconWrap = styled.View({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 24,
+  height: 24,
+  marginTop: 6,
+  marginBottom: 3,
+});
+
+export const textWrap = css({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginBottom: 9,
+});
