324fbc6e62f2a2eb68db7ec05d5767749accc988
kanghyun98 <lkh981107@gmail.com>
2022-06-25 14:03:56 +0900

    feat: add useInfiniteScroll custom hook

---
+++ projects/sasil-web/src/logics/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,40 @@
+import { useRef, useEffect, MutableRefObject, RefObject } from 'react';
+
+type callbackFuncType = (...args: any) => any;
+
+const useInifiniteScroll = (
+  containerRef: RefObject<HTMLDivElement>,
+  callbackFunc: callbackFuncType,
+) => {
+  // Node.js 환경에서 빌드됨으로써 생기는 오류 해결하기 위한 참조
+  const observerRef: MutableRefObject<IntersectionObserver | null> =
+    useRef<IntersectionObserver>(null);
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver(
+      (entries, curObserver) => {
+        if (entries[0].isIntersecting) {
+          callbackFunc(); // 여기서 전역 상태를 업데이트 해야함
+
+          curObserver.disconnect(); // 관측 해제
+        }
+      },
+      {
+        threshold: 1,
+      },
+    );
+
+    if (!containerRef.current?.lastElementChild) {
+      return;
+    }
+
+    observerRef.current.observe(containerRef.current.lastElementChild);
+
+    // TODO: understand, solve typescript error
+    return () => {
+      observerRef.current?.disconnect();
+    };
+  }, [callbackFunc, containerRef]);
+};
+
+export default useInifiniteScroll;
