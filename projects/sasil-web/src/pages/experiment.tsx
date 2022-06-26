214e82f93e85168742a6f91560e62c5d0fd4e867
kanghyun98 <lkh981107@gmail.com>
2022-06-26 22:22:37 +0900

    feat: add experiment page with infinite scroll

--- projects/sasil-web/src/pages/experiment.tsx
+++ projects/sasil-web/src/pages/experiment.tsx
@@ -1,20 +1,58 @@
+import React, { useRef } from 'react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useInfiniteQuery } from 'react-query';
 
+import { categories } from 'src/dummyData';
 import { getPostsAsync, SortType } from '@sasil/common';
-
-import { categories, expPosts } from 'src/dummyData';
 import ReqExpTemplate from '@/components/templates/ReqExpTemplate';
+import useInifiniteScroll from '@/logics/hooks/useInfiniteScroll';
 
 const ExperimentPage: NextPage = () => {
   const router = useRouter();
 
-  const pageType = 'experiment';
   const sortType = (router?.query?.sort || 'recent') as SortType;
-  const display = 12;
+  const pageType = 'experiment';
+  const display = 1; // TODO: 몇 개씩 표시될건지 정하기!
+
+  const postsRef = useRef(null);
+
+  const { data, isFetching, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    ['posts', { pageType, sortType }],
+    ({ pageParam = 1 }) =>
+      getPostsAsync(pageType, pageParam, display, sortType),
+    {
+      getNextPageParam: (lastPage) => {
+        if (!lastPage.isSuccess || lastPage.result.isLast) {
+          return undefined;
+        }
+
+        return lastPage.result.nextPage;
+      },
+      staleTime: 300000, // TODO: 일단 cache-time의 default 값인 5분과 동일하게 맞춤
+    },
+  );
+
+  const getExpPosts = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  useInifiniteScroll(postsRef, getExpPosts);
+
+  const postsData = data?.pages
+    .map((res) => (res.isSuccess ? res.result.posts : []))
+    .flat();
 
   return (
-    <ReqExpTemplate type={pageType} posts={expPosts} categories={categories} />
+    <ReqExpTemplate
+      postsRef={postsRef}
+      type={pageType}
+      sortType={sortType}
+      posts={postsData}
+      categories={categories}
+    />
   );
 };
 
