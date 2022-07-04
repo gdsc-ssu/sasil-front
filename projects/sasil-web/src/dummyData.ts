72dbcfc456dddc58cac6b58ad60c428d381bb11e
ballsona0925 <ballsona0925@naver.com>
2022-07-04 16:25:33 +0900

    feat: modify dummyDatat in web

--- projects/sasil-web/src/dummyData.ts
+++ projects/sasil-web/src/dummyData.ts
@@ -25,6 +25,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 2,
@@ -36,6 +37,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 3,
@@ -46,6 +48,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 4,
@@ -56,6 +59,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 5,
@@ -67,6 +71,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 6,
@@ -77,6 +82,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 7,
@@ -88,6 +94,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 8,
@@ -99,6 +106,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 9,
@@ -110,6 +118,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 10,
@@ -121,6 +130,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
   {
     id: 11,
@@ -131,6 +141,7 @@ export const expPosts: PostInfoType[] = [
     user: users[0],
     categories: [],
     likeCount: 9,
+    bookmarkCount: 25,
   },
 ];
 
@@ -146,6 +157,205 @@ export const reqPosts: PostInfoType[] = [
     categories: [],
     state: 'answered',
     likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 2,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 3,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 4,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 5,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'answered',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 6,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 7,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'answered',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 8,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 9,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 10,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'answered',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+  {
+    id: 11,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'wait',
+    likeCount: 25,
+    bookmarkCount: 25,
+  },
+];
+
+export const categories: CategoryType[] = [
+  { id: 1, name: '프론트엔드' },
+  { id: 2, name: '백엔드' },
+  { id: 3, name: 'Javascript' },
+  { id: 4, name: 'Typescript' },
+  { id: 5, name: 'HTML' },
+  { id: 6, name: 'CSS' },
+  { id: 7, name: 'Storybook' },
+];
+
+export const reqPostDetail = {
+  id: 9,
+  created_at: new Date('2022-05-19T14:53:43.044Z'),
+  updated_at: new Date('2022-05-19T14:53:43.044Z'),
+  user: users[1],
+  title: '게시물 제목 예시입니다',
+  thumbnail:
+    'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+  content:
+    '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>',
+  likeCount: 9,
+  bookmarkCount: 25,
+  liked: false,
+  bookmarked: false,
+  categories: [
+    { name: '카테고리1', id: 1 },
+    { name: '카테고리2', id: 2 },
+    { name: '카테고리3', id: 3 },
+  ],
+  comments: [
+    {
+      id: 1,
+      writerObj: { nickname: '작성자1', profileImg: null },
+      content: '안녕하세요 댓글이에요 1',
+    },
+    {
+      id: 2,
+      writerObj: { nickname: '작성자2', profileImg: null },
+      content: '안녕하세요  댓글이에요 2',
+    },
+    {
+      id: 3,
+      writerObj: { nickname: '작성자3', profileImg: null },
+      content: '안녕하세요  댓글이에요 3',
+    },
+    {
+      id: 4,
+      writerObj: { nickname: '작성자4', profileImg: null },
+      content: '안녕하세요  댓글이에요 4',
+    },
+  ],
+};
+
+export const reqAnswerPosts = [
+  {
+    id: 1,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[1],
+    categories: [],
+    state: 'answered',
+    likeCount: 25,
   },
   {
     id: 2,
@@ -183,98 +393,62 @@ export const reqPosts: PostInfoType[] = [
     state: 'wait',
     likeCount: 25,
   },
-  {
-    id: 5,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'answered',
-    likeCount: 25,
-  },
-  {
-    id: 6,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'wait',
-    likeCount: 25,
-  },
-  {
-    id: 7,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'answered',
-    likeCount: 25,
-  },
-  {
-    id: 8,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'wait',
-    likeCount: 25,
-  },
-  {
-    id: 9,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'wait',
-    likeCount: 25,
-  },
-  {
-    id: 10,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'answered',
-    likeCount: 25,
-  },
-  {
-    id: 11,
-    created_at: new Date('2022-05-19T14:53:43.044Z'),
-    updated_at: new Date('2022-05-19T14:53:43.044Z'),
-    title: 'testPost',
-    thumbnail:
-      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
-    user: users[1],
-    categories: [],
-    state: 'wait',
-    likeCount: 25,
-  },
 ];
 
-export const categories: CategoryType[] = [
-  { id: 1, name: '프론트엔드' },
-  { id: 2, name: '백엔드' },
-  { id: 3, name: 'Javascript' },
-  { id: 4, name: 'Typescript' },
-  { id: 5, name: 'HTML' },
-  { id: 6, name: 'CSS' },
-  { id: 7, name: 'Storybook' },
+export const expPostDetail = {
+  id: 9,
+  created_at: new Date('2022-05-19T14:53:43.044Z'),
+  updated_at: new Date('2022-05-19T14:53:43.044Z'),
+  user: users[1],
+  title: '게시물 제목 예시입니다',
+  thumbnail:
+    'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+  content:
+    '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>',
+  likeCount: 9,
+  bookmarkCount: 25,
+  liked: false,
+  bookmarked: false,
+  categories: [
+    { name: '카테고리1', id: 1 },
+    { name: '카테고리2', id: 2 },
+    { name: '카테고리3', id: 3 },
+  ],
+  comments: [
+    {
+      id: 1,
+      writerObj: { nickname: '작성자1', profileImg: null },
+      content: '안녕하세요 댓글이에요 1',
+    },
+    {
+      id: 2,
+      writerObj: { nickname: '작성자2', profileImg: null },
+      content: '안녕하세요  댓글이에요 2',
+    },
+    {
+      id: 3,
+      writerObj: { nickname: '작성자3', profileImg: null },
+      content: '안녕하세요  댓글이에요 3',
+    },
+    {
+      id: 4,
+      writerObj: { nickname: '작성자4', profileImg: null },
+      content: '안녕하세요  댓글이에요 4',
+    },
+  ],
+};
+
+export const expRequestPost = [
+  {
+    id: 1,
+    created_at: new Date('2022-05-19T14:53:43.044Z'),
+    updated_at: new Date('2022-05-19T14:53:43.044Z'),
+    title: 'testPost1',
+    thumbnail:
+      'https://image.shutterstock.com/image-photo/cute-labrador-dog-playing-stick-600w-1935251336.jpg',
+    user: users[0],
+    categories: [],
+    likeCount: 9,
+    bookmarkCount: 25,
+  },
 ];
