e716a4d96867bb6ebfce8d65ddab005dbb8878a9
ballsona0925 <ballsona0925@naver.com>
2022-07-04 16:20:21 +0900

    feat: add `Comment` component

---
+++ projects/sasil-web/src/components/organisms/Comment/Comment.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Comment, { CommentProps } from '.';
+
+export default {
+  title: 'components/organism/Comment',
+  component: Comment,
+} as ComponentMeta<typeof Comment>;
+
+const Template: ComponentStory<typeof Comment> = ({
+  writerObj,
+  content,
+}: CommentProps) => <Comment writerObj={writerObj} content={content} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  writerObj: {
+    nickname: '작성자',
+    profileImg: null,
+  },
+  content: '댓글댓글',
+};
---
+++ projects/sasil-web/src/components/organisms/Comment/Comment.style.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const CommentWrap = styled.div({
+  width: '100%',
+  height: '67px',
+  padding: '10px 12px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'space-between',
+});
+
+export const Top = styled.div({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+});
